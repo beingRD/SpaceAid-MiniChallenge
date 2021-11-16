624c4595b1e63bc85d91c581856470bf4616878a
Nicolas Mariniello <seldon1000@tutanota.com>
2021-11-16 15:48:55 +0100

    Added initial 'About Us' tab content

--- SpaceAidExplorer/ui/tabs/aboutus/AboutUsTab.swift
+++ SpaceAidExplorer/ui/tabs/aboutus/AboutUsTab.swift
@@ -9,6 +9,18 @@ import SwiftUI
 
 struct AboutUsTab: View {
     var body: some View {
-        Text("Ciao")
+        VStack {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 128, height: 128)
+                .padding(64)
+            List {
+                Link("Nicolas Mariniello", destination: URL(string: "https://www.github.com/seldon1000")!)
+                Link("Rishabh Dev", destination: URL(string: "https://www.github.com/beingRD")!)
+                Link("Salvatore Gallo", destination: URL(string: "https://www.github.com/seldon1000")!)
+                Link("Raffaele Colantonio", destination: URL(string: "https://www.github.com/Rafas02")!)
+            }
+        }
     }
 }
